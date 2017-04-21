1cf3284b259b28b7bf7fd9cc9830ecb7e77eaa98
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-04-21 07:58:59 +0200

    Initial Commit

---
+++ DBEmptyState/DBEmptyState.h
@@ -0,0 +1,19 @@
+//
+//  DBEmptyState.h
+//  DBEmptyState
+//
+//  Created by Lukas Schmidt on 21.04.17.
+//  Copyright © 2017 DBSystel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DBEmptyState.
+FOUNDATION_EXPORT double DBEmptyStateVersionNumber;
+
+//! Project version string for DBEmptyState.
+FOUNDATION_EXPORT const unsigned char DBEmptyStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DBEmptyState/PublicHeader.h>
+
+
