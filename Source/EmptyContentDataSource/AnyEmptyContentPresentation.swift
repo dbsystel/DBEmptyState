c6a515189a1f031a02e6c9013e1354ac06fdf34b
Dennis Post <dennis.post@deutschebahn.com>
2018-06-28 09:16:55 +0200

    Fix optionals

--- Source/EmptyContentDataSource/AnyEmptyContentPresentation.swift
+++ Source/EmptyContentDataSource/AnyEmptyContentPresentation.swift
@@ -8,9 +8,9 @@
 
 public class AnyEmptyContentPresentation<EmptyState: Equatable>: EmptyContentPresentationDelegate {
     
-    private let shouldAllowTouch: (EmptyState) -> Bool?
-    private let shouldAllowScroll: (EmptyState) -> Bool?
-    private let shouldAllowImageViewAnimate: (EmptyState) -> Bool?
+    private let shouldAllowTouch: (EmptyState) -> Bool
+    private let shouldAllowScroll: (EmptyState) -> Bool
+    private let shouldAllowImageViewAnimate: (EmptyState) -> Bool
     
     public init<D: EmptyContentPresentationDelegate>(_ emptyContentPresentationDelegate: D) where D.EmptyState == EmptyState {
         unowned let weakEmptyContentPresentationDelegate = emptyContentPresentationDelegate
@@ -19,15 +19,15 @@ public class AnyEmptyContentPresentation<EmptyState: Equatable>: EmptyContentPre
         shouldAllowImageViewAnimate = { weakEmptyContentPresentationDelegate.shouldAllowImageViewAnimate(for: $0) }
     }
     
-    public func shouldAllowScroll(for state: EmptyState) -> Bool? {
+    public func shouldAllowScroll(for state: EmptyState) -> Bool {
         return shouldAllowScroll(state)
     }
     
-    public func shouldAllowTouch(for state: EmptyState) -> Bool? {
+    public func shouldAllowTouch(for state: EmptyState) -> Bool {
         return shouldAllowTouch(state)
     }
     
-    public func shouldAllowImageViewAnimate(for state: EmptyState) -> Bool? {
+    public func shouldAllowImageViewAnimate(for state: EmptyState) -> Bool {
         return shouldAllowImageViewAnimate(state)
     }
 }
